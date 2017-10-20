510187ddcfd6947830a5fa7e746c517c6865f262
Christopher Grace & Karlie & Neema Shekar & Zach Broaddus <pair+chrisgrace3.karlieromas.neemashekar.zachbroaddus@devbootcamp.com>
2017-10-19 21:27:32 -0500

    fix seed file errors and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
 
-f_name = Faker::Name.first_name
-l_name = Faker::Name.last_name
-email = Faker::Internet.safe_email
-user = Faker::Internet.user_name(f_name, 8..15)
-pass = Faker::Internet.password(12)
+# f_name = Faker::Name.first_name
+# l_name = Faker::Name.last_name
+# email = Faker::Internet.safe_email
+# user = Faker::Internet.user_name(8..15)
+# pass = Faker::Internet.password(12)
 
-50.times { User.create!(first_name: f_name, last_name: l_name, email: email, username: user, password: pass) }
-# 10.times { User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, username: Faker::Internet.user_name(8..15), password: Faker::Internet.password(12)) }
+# 50.times { User.create!(first_name: f_name, last_name: l_name, email: email, username: user, password: pass) }
+50.times { User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, username: Faker::Internet.user_name(8..15), password: Faker::Internet.password(12)) }
 
 
-Post.create!(photo_url: "https://tr2.cbsistatic.com/hub/i/2014/05/15/f8964afd-bd82-4e0e-bcbe-e927363dcdc1/3b858e39e2cf183b878f54cad0073a67/codedoge.jpg", vote_tally: rand(1..200), user_id: 20)
 Post.create!(photo_url: "https://static.tumblr.com/06ccf3b8566d2de1879f620987e30bec/nayuswz/6Ctosfaei/tumblr_static_tumblr_static_9al0ol6aee4gk8084sss8cw8o_640.jpg", vote_tally: rand(1..200),  question: "You gonna eat that?", user_id: 1)
 Post.create!(photo_url: "https://i.imgflip.com/1c8yl4.jpg", question: "How can I help you?", vote_tally: rand(1..200), user_id: 2)
 Post.create!(photo_url: "https://www.puppyleaks.com/wp-content/uploads/2015/04/smilinggreyhound.png", question: "What's so funny?", user_id: 3)
@@ -30,6 +29,7 @@ Post.create!(photo_url: "https://pics.me.me/smooth-doggo-17465058.png", question
 Post.create!(photo_url: "https://media.giphy.com/media/Z3aQVJ78mmLyo/source.gif", question: "How do you get a pupper to calm down?", vote_tally: rand(1..200),  user_id: 17)
 Post.create!(photo_url: "http://vignette3.wikia.nocookie.net/soma/images/2/29/18361-doge-follow-your-dreams.jpg/revision/latest?cb=20151028152235", question: "Should I encourage my doggo to pursue her PhD??", vote_tally: rand(1..200),  user_id: 18)
 Post.create!(photo_url: "https://www.shelterluv.com/sites/default/files/animal_pics/464/2017/06/23/21/20170623212057.png", question: "My dog's tongue appears to be broken. Has anyone seen this before?", vote_tally: rand(1..200),  user_id: 19)
+Post.create!(photo_url: "https://tr2.cbsistatic.com/hub/i/2014/05/15/f8964afd-bd82-4e0e-bcbe-e927363dcdc1/3b858e39e2cf183b878f54cad0073a67/codedoge.jpg", vote_tally: rand(1..200), question: "Smartest doggo ever?", user_id: 20)
 
 
 
@@ -56,27 +56,27 @@ Answer.create!(user_id: rand(1..50), post_id: rand(1..20), description_text: "D'
 Answer.create!(user_id: rand(1..50), post_id: rand(1..20), description_text: "That doggo owes me $20!")
 
 
-Comment.create!(description_text: "Proud of you!", commentable_type: "answer", user_id: 1, commentable_id: 1)
-Comment.create!(description_text: "Niiiiice", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "Whoa!", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "Geeeez", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "That doggo owes me $20!", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "That doge knows how to party!", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "I need moar doggos in my life.", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "This pupper reminds me of my old dog, Maria Blunderfield-Wardyworth", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "2 BITS", commentable_type: "answer", user_id: , commentable_id: )
-Comment.create!(description_text: "Yasssssss!", commentable_type: "answer", user_id: , commentable_id: )
+Comment.create!(description_text: "Proud of you!", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "Niiiiice", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "Whoa!", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "Geeeez", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "That doggo owes me $20!", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "That doge knows how to party!", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "I need moar doggos in my life.", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "This pupper reminds me of my old dog, Maria Blunderfield-Wardyworth",commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "2 BITS", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
+Comment.create!(description_text: "Yasssssss!", commentable_type: "answer", user_id: rand(1..50), commentable_id: rand(1..21))
 
-Comment.create!(description_text: "This doggo helped me fix my router. 10/10, would recommend", commentable_type: "post", user_id: 5, commentable_id: 2)
-Comment.create!(description_text: "Where can I get a cute doggo like that?!", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "D'awwwwww!!", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "I need moar doggos in my life.", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "This pupper reminds me of my old dog, Maria Blunderfield-Wardyworth", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "2 BITS", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "Yasssssss!", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "So many doggos to pet, so little time...", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "I dropped my phone in the toilet. Is this the right thread...? HALP!", commentable_type: "post", user_id: , commentable_id: )
-Comment.create!(description_text: "I lost 80 pounds in ten minutes with this simple trick. Go to www.identitytheft.scam", commentable_type: "post", user_id: , commentable_id: )
+Comment.create!(description_text: "This doggo helped me fix my router. 10/10, would recommend", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "Where can I get a cute doggo like that?!", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "D'awwwwww!!", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "I need moar doggos in my life.", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "This pupper reminds me of my old dog, Maria Blunderfield-Wardyworth", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "2 BITS", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "Yasssssss!", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "So many doggos to pet, so little time...", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "I dropped my phone in the toilet. Is this the right thread...? HALP!", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
+Comment.create!(description_text: "I lost 80 pounds in ten minutes with this simple trick. Go to www.identitytheft.scam", commentable_type: "post", user_id: rand(1..50), commentable_id: rand(1..20))
 
 
 Vote.create!(votable_type: "post", votable_id: 1, user_id: 2)
