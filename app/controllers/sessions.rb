45438984bcdf76e3cb2e15cb8bc813c2fdfa2df8
Karlie Romas <karlie.romas@gmail.com>
2017-10-20 10:07:56 -0500

    sign up, log in and logout is up and running

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,15 +1,18 @@
 get '/sessions/new' do
-  @previous_page = request.referrer
   erb :'sessions/new'
 end
 
 post '/sessions' do
   @user = User.find_by(username: params[:username])
-  if @user && @user.authenticate(params[:password])
+  if @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect '/'
   else
-    @errors = ["Either your username or password was wrong, fren"]
     erb :'sessions/new'
   end
+end
+
+delete '/sessions' do
+  session[:user_id] = nil
+  redirect '/'
 end
