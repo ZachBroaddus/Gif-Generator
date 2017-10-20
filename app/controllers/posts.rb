01cf9f1cf19a61a95899682e98d412f2ee22c7df
Christopher Grace & Neema Shekar <pair+chrisgrace3.neemashekar@devbootcamp.com>
2017-10-20 17:46:48 -0500

    add new post form and verify it works

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -3,16 +3,20 @@ get '/posts' do
   erb :'posts/index'
 end
 
+get '/posts/new' do
+  erb :"posts/new"
+end
+
 get '/posts/:id' do
   @post = Post.find_by(id: params[:id])
   erb :'posts/show'
 end
 
-get '/posts/new' do
-  erb :"posts/new"
-end
 
-post 'posts' do
-  Post.new()
+post '/posts' do
+  # p params[:picture]
+  Post.create(photo_url: params[:picture], question: params[:question], user_id: session[:user_id])
+
+  redirect "/"
 end
 
