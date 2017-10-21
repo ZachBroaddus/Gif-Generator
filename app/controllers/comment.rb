a4b3b5e24255eda5dda4e0e9745cafd4f22dab8d
Christopher Grace & Neema Shekar <pair+chrisgrace3.neemashekar@devbootcamp.com>
2017-10-20 20:36:29 -0500

    add comment links to questions and answers, create comment form, routes are still in progress

---
+++ app/controllers/comment.rb
@@ -0,0 +1,10 @@
+# get 'comments/new' do
+#   erb :'comments/new'
+# end
+
+# post "/posts/#{@post.id}/comments"do
+#   @post = Post.find(params[:post_id])
+#   @comment = Comment.create(description_text: params[:description_text], user_id: session[:user_id])
+
+#   redirect "/post/#{@post.id}"
+# end
