364f2bb2fdf12aee20bd93a9fef1e8735c5007d9
Zach Broaddus <zach.broaddus@gmail.com>
2017-10-21 16:42:33 -0500

    fix ajax errors on post buttons and redirect users to sign up if not logged in

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,3 +1,50 @@
+# get '/posts' do
+#   @posts = Post.all
+#   erb :'posts/index'
+# end
+
+# get '/posts/new' do
+#   erb :"posts/new"
+# end
+
+# get '/posts/:id' do
+#   @post = Post.find_by(id: params[:id])
+#   erb :'posts/show'
+# end
+
+
+# post '/posts' do
+#   @post = Post.create(photo_url: params[:picture], question: params[:question], user_id: session[:user_id], vote_tally: 0)
+
+#   redirect "/posts/#{@post.id}"
+# end
+
+# post '/posts/:id/vote' do
+#   post = Post.find(params[:id])
+
+#     p "IS AN AJAX REQUEST?"
+#     p request.xhr?
+#   post.votes.create(votable_type: "post", votable_id: post.id, user_id: session[:user_id])
+
+#   if request.xhr? && logged_in?
+#     post.votes.count.to_s
+#   else
+#     redirect "/posts/#{post.id}"
+#   end
+# end
+
+# delete '/posts/:id/vote' do
+#   post = Post.find(params[:id])
+#   post.votes.last.destroy
+#   if request.xhr? && logged_in?
+#     post.votes.count.to_s
+#   else
+#     redirect "/posts/#{post.id}"
+#   end
+# end
+
+
+
 get '/posts' do
   @posts = Post.all
   erb :'posts/index'
@@ -21,12 +68,17 @@ end
 
 post '/posts/:id/vote' do
   post = Post.find(params[:id])
-  post.votes.create(votable_type: "post", votable_id: post.id, user_id: session[:user_id])
-  if request.xhr? && logged_in?
-    post.votes.count.to_s
-  else
-    redirect "/posts/#{post.id}"
+
+  # p "THIS IS AN AJAX REQUEST"
+  # p request.xhr?
+
+  if logged_in?
+    if request.xhr?
+      post.votes.create(votable_type: "post", votable_id: post.id, user_id: session[:user_id])
+      post.votes.count.to_s
+    end
   end
+    redirect "/posts/#{post.id}"
 end
 
 delete '/posts/:id/vote' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,32 @@
+// $(document).ready(function() {
+//   // This is called after the document has loaded in its entirety
+//   // This guarantees that any elements we bind to will exist on the page
+//   // when we try to bind to them
+
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+//   $(".post-container").on("submit", ".inline-vote-form-upvote", function(event) {
+//     event.preventDefault();
+//     var $form = $(".inline-vote-form-upvote");
+//     var actiony = $form.attr("action");
+//     var ajaxRequest = $.ajax({url: actiony, method: 'POST'});
+//     ajaxRequest.done(function(response){
+//       $form.closest("article").find(".points").text(response);
+//       $form.find(".upvote-button").css("color", "#8c4331");
+//     });
+
+
+//   $(".post-container").on("submit", ".inline-vote-form-downvote", function(event) {
+//     event.preventDefault();
+//     var $form = $(".inline-vote-form-downvote");
+//     var actiony = $form.attr("action");
+//     var ajaxRequest = $.ajax({url: actiony, method: 'DELETE'});
+//     ajaxRequest.done(function(response){
+//       $form.closest("article").find(".points").text(response);
+//       $form.find(".downvote-button").css("color", "#8c4331");
+//     });
+//   });
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
@@ -26,21 +55,5 @@ $(document).ready(function() {
       $form.find(".downvote-button").css("color", "#8c4331");
     });
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 });
