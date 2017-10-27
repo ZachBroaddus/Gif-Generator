fcd68a72368514ad356eafb7a80e296fea662838
Zach Broaddus <zach.broaddus@gmail.com>
2017-10-27 11:51:32 -0500

    Everything works! Time to style

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,13 +1,14 @@
 get '/' do
-  @giphy = Giph.create
-  response = @giphy.get_api_response
-  p @giphy.image_url = response['data'][rand(0..4)]['images']['fixed_height']['url']
-
   erb :'/index'
 end
 
 post '/' do
+  @giphy = Giph.create
+  keyword = params[:keyword]
+  response = @giphy.get_api_response(keyword)
+  @giphy.image_url = response['data'][rand(0..4)]['images']['original']['url']
 
+  erb :'/index'
 end
 
 
--- app/models/giph.rb
+++ app/models/giph.rb
@@ -1,26 +1,13 @@
 class Giph < ActiveRecord::Base
-  # Remember to create a migration!
-  # validates :image_url, presence: true
 
-  def get_api_response
+  def get_api_response(keyword)
     # Remove API_KEY from code before pushing
     api_key = "fqAzP5UHkbdp2ITsnM5Cm3Zn0iHJUcTg"
 
-    url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=#{api_key}&limit=5"
+    url = "http://api.giphy.com/v1/gifs/search?q=#{keyword}&api_key=#{api_key}&rating=pg-13&limit=5"
     resp = Net::HTTP.get_response(URI.parse(url))
     buffer = resp.body
     result = JSON.parse(buffer)
-    # puts result
-    # uri = URI(image_url)
-    # api_response = Net::HTTP.get(uri)
-    # response_collection = JSON.parse(api_response)
   end
-
-  # def make_giphs
-  #   p get_api_response
-  # end
 end
 
-@giphy = Giph.create
-response = @giphy.get_api_response
-@giphy.image_url = response['data'][rand(0..4)]['url']
