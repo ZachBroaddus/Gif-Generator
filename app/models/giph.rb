499faba7ce053e9541b7913b7b2b117e1c37326f
Zach Broaddus <zach.broaddus@gmail.com>
2017-10-27 17:47:25 -0500

    done...for now

--- app/models/giph.rb
+++ app/models/giph.rb
@@ -1,7 +1,8 @@
 class Giph < ActiveRecord::Base
 
   def get_api_response(keyword)
-    url = "http://api.giphy.com/v1/gifs/search?q=#{keyword}&API_KEY=#{API_KEY}&rating=pg-13&limit=5"
+
+    url = "http://api.giphy.com/v1/gifs/search?q=#{keyword}&api_key=#{api_key}&rating=pg-13&limit=5"
     resp = Net::HTTP.get_response(URI.parse(url))
     buffer = resp.body
     result = JSON.parse(buffer)
